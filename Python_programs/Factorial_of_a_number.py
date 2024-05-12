d48e00c959e00644aa811963d66c4d77d0bf2687
Rahul-Barde <140281547+Rahul-Barde@users.noreply.github.com>
2024-05-12 08:35:10 +0530

    Added docs for git and pytest

--- Python_programs/Factorial_of_a_number.py
+++ Python_programs/Factorial_of_a_number.py
@@ -1,12 +1,21 @@
-num = int(input('Enter a number: '))
+import math
 
 factorial = 1
 
-if num <0:
-    print('you have entered a negative number')
-elif num == 0 :
-    print('you have entered 0')
-else:
-    for i in range (1,num + 1):
-        factorial = factorial*i
-    print('factorial of',num,' is ', factorial)
+def factorial(n):
+    if n <0:
+        print('you have entered a negative number')
+    elif n == 0 :
+        print('you have entered 0')
+    else:
+        for i in range (1,n + 1):
+            s *= s
+        print('factorial of',n,' is ', s)
+
+def factorial_math(n):
+    print('factorial of',n,' is ',math.factorial(n))
+
+#taking user input
+num = int(input('Enter a number: '))
+
+factorial_math(num)
